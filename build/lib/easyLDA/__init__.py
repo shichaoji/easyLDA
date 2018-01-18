b3e4f2a5734a2f8e7f4b4ca7c87fd102b34a3f57
Shichao Ji <jshichao@vt.edu>
2018-01-18 18:38:12 -0500

    optimize pipline

--- build/lib/easyLDA/__init__.py
+++ build/lib/easyLDA/__init__.py
@@ -1,17 +1,22 @@
 # coding: utf-8
-import sys
-try:
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
-except:
-    print('python3')
+
+#import sys
+#try:
+#    reload(sys)
+#    sys.setdefaultencoding("utf-8")
+#except:
+#    print('python3')
     
+import nltk
 try:
-    import nltk
+    from nltk.corpus import stopwords
+    from nltk.stem import wordnet
+    print 'stopword & wordnet data downloaded already'
+except:
+    print 'downloading nltk data'
     nltk.download("stopwords")
     nltk.download("wordnet")
-except:
-    'stopword data downloaded already'
+
     
 from .base import PipelineLDA
 from .base import main as LDA_main
